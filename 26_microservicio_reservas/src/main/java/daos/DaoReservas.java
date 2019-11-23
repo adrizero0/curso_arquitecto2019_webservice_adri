93d68f04ef4945bd6fa7198272c938ab00f260e5
Enrique <Enrique@DESKTOP-LUIS>
2019-11-23 19:01:37 +0100

    comm2

--- 26_microservicio_reservas/src/main/java/daos/DaoReservas.java
+++
@@ -1,9 +0,0 @@
-package daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Reserva;
-
-public interface DaoReservas extends JpaRepository<Reserva, Integer> {	
-	
-}
